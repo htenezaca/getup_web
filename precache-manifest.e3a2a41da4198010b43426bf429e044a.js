e016aca851ea5ee912e667465d932d0722fc01b3
Hector Tenezaca <h.tenezaca@uniandes.edu.co>
2023-03-16 00:18:22 -0500

    Updates

---
+++ precache-manifest.e3a2a41da4198010b43426bf429e044a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bec1b12f5b9dc95a35f7",
+    "url": "/htenezaca/getup_web/static/css/main.752fbdb6.chunk.css"
+  },
+  {
+    "revision": "bec1b12f5b9dc95a35f7",
+    "url": "/htenezaca/getup_web/static/js/main.bec1b12f.chunk.js"
+  },
+  {
+    "revision": "041adcc4004a731599c1",
+    "url": "/htenezaca/getup_web/static/js/1.041adcc4.chunk.js"
+  },
+  {
+    "revision": "adcdbfbd0a464dfef687",
+    "url": "/htenezaca/getup_web/static/css/2.8574b1f9.chunk.css"
+  },
+  {
+    "revision": "adcdbfbd0a464dfef687",
+    "url": "/htenezaca/getup_web/static/js/2.adcdbfbd.chunk.js"
+  },
+  {
+    "revision": "785c4ab15fab715d91db",
+    "url": "/htenezaca/getup_web/static/js/runtime~main.785c4ab1.js"
+  },
+  {
+    "revision": "3d49d43b7f3fc8932ce506e6bcb5f672",
+    "url": "/htenezaca/getup_web/index.html"
+  }
+];
